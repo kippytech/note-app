f4e08fe3c9fe4e34b5f7afdf00ddfda774fa8e32
kippytech <dkipyegon.dk7.dk@gmail.com>
2024-01-24 12:16:42 +0300

    add metadata

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,10 @@
 import { SignIn } from "@clerk/nextjs";
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "brainy - sign-in",
+};
 
 function SignInPage() {
   return (
