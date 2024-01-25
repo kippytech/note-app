af8c3e9345932cb9d09c6ac3c47a3f03a914190c
kippytech <dkipyegon.dk7.dk@gmail.com>
2024-01-25 09:18:04 +0300

    added logo

--- src/app/notes/Navbar.tsx
+++ src/app/notes/Navbar.tsx
@@ -19,7 +19,7 @@ function Navbar() {
       <div className="p-4 shadow">
         <div className="mx-auto flex max-w-7xl flex-wrap items-center justify-between gap-3">
           <Link href="/notes" className="flex items-center gap-1">
-            <Image src="/brainy.png" alt="brainy logo" height={40} width={40} />
+            <Image src="/logo.png" alt="brainy logo" height={40} width={40} />
             <span className="font-bold">brainy</span>
           </Link>
           <div className="flex items-center gap-2">
