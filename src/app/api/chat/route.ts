fc0d8f8ba68c7f7f6581fec8345264abb2162972
kippytech <dkipyegon.dk7.dk@gmail.com>
2024-01-25 15:50:48 +0300

    add chat api route

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,60 @@
+import prisma from "@/lib/db/prisma";
+import openai, { getEmbedding } from "@/lib/openai";
+import { notesIndex } from "@/lib/vectordb";
+import { auth } from "@clerk/nextjs";
+import { ChatCompletionMessage } from "openai/resources/index.mjs";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const messages: ChatCompletionMessage[] = body.messages;
+
+    const truncatedMessages = messages.slice(-6);
+
+    const embedding = await getEmbedding(
+      truncatedMessages.map((mssg) => mssg.content).join("\n"),
+    );
+
+    const { userId } = auth();
+
+    const vectorQueryResponse = await notesIndex.query({
+      vector: embedding,
+      topK: 4,
+      filter: { userId },
+    });
+
+    const relevantNotes = await prisma.note.findMany({
+      where: {
+        id: {
+          in: vectorQueryResponse.matches.map((match) => match.id),
+        },
+      },
+    });
+
+    const systemMessage: ChatCompletionMessage = {
+      role: "assistant",
+      content:
+        "You are an intelligent note-taking app. You answer the users's question based on their existing notes. " +
+        "The relevant notes for this query are:\n" +
+        relevantNotes
+          .map((note) => `Title: ${note.title}\n\nContent: ${note.content}`)
+          .join("\n\n"),
+    };
+
+    const res = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      temperature: 0,
+      stream: true,
+      messages: [systemMessage, ...truncatedMessages],
+    });
+
+    const stream = OpenAIStream(res);
+
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
