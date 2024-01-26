17f7a8a42aaa869d9bda184f8a22105d945b05fa
kippytech <dkipyegon.dk7.dk@gmail.com>
2024-01-26 08:04:51 +0300

    complete chat streaming

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -1,8 +1,12 @@
 import { cn } from "@/lib/utils";
 import { useChat } from "ai/react";
-import { XCircle } from "lucide-react";
+import { Bot, Trash, XCircle } from "lucide-react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { Message } from "ai";
+import { useUser } from "@clerk/nextjs";
+import Image from "next/image";
+import { useEffect, useRef } from "react";
 
 interface ChatBoxProps {
   open: boolean;
@@ -20,6 +24,23 @@ export default function ChatBox({ open, onClose }: ChatBoxProps) {
     error,
   } = useChat();
 
+  const inputRef = useRef<HTMLInputElement>(null);
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+    }
+  }, [messages]);
+
+  useEffect(() => {
+    if (open) {
+      inputRef.current?.focus();
+    }
+  }, [open]);
+
+  const lastMessageIsUser = messages[messages.length - 1]?.role === "user";
+
   return (
     <div
       className={cn(
@@ -31,16 +52,87 @@ export default function ChatBox({ open, onClose }: ChatBoxProps) {
         <XCircle size={30} />
       </button>
       <div className="flex h-[600px] flex-col rounded border-2 bg-background shadow-xl">
-        <div className="h-full">Messages</div>
+        <div className="h-full overflow-y-auto px-3" ref={scrollRef}>
+          {messages.map((mssg) => (
+            <ChatMessage message={mssg} key={mssg.id} />
+          ))}
+          {isLoading && lastMessageIsUser && (
+            <ChatMessage
+              message={{ role: "assistant", content: "Brainy is thinking..." }}
+            />
+          )}
+          {error && (
+            <ChatMessage
+              message={{
+                role: "assistant",
+                content: "Something went wrong. Please try again.",
+              }}
+            />
+          )}
+          {!error && messages.length === 0 && (
+            <div className="flex h-full items-center justify-center gap-3">
+              <Bot />
+              Ask the AI a question about your notes.
+            </div>
+          )}
+        </div>
         <form onSubmit={handleSubmit} className="m-3 flex gap-1">
+          <Button
+            onClick={() => setMessages([])}
+            title="Clear chat"
+            variant="outline"
+            size="icon"
+            className="shrink-0"
+            type="button"
+          >
+            <Trash />
+          </Button>
           <Input
             value={input}
             onChange={handleInputChange}
             placeholder="Chat..."
+            ref={inputRef}
           />
           <Button type="submit">Send</Button>
         </form>
       </div>
+    </div>
+  );
+}
+
+function ChatMessage({
+  message: { role, content },
+}: {
+  message: Pick<Message, "role" | "content">;
+}) {
+  const { user } = useUser();
+
+  const isAIMessage = role === "assistant";
+  return (
+    <div
+      className={cn(
+        "mb-3 flex items-center",
+        isAIMessage ? "me-5 justify-start" : "ms-5 justify-end",
+      )}
+    >
+      {isAIMessage && <Bot className="mr-2 shrink-0" />}
+      <p
+        className={cn(
+          "whitespace-pre-line rounded-md border px-3 py-2",
+          isAIMessage ? "bg-background" : "bg-primary text-primary-foreground",
+        )}
+      >
+        {content}
+      </p>
+      {!isAIMessage && user?.imageUrl && (
+        <Image
+          src={user.imageUrl}
+          alt="user image"
+          width={40}
+          height={40}
+          className="ml-2 aspect-square rounded-full"
+        />
+      )}
     </div>
   );
 }
