c86679a6c1dd7e8a7b8545427a23c04b27b6bcf8
kippytech <dkipyegon.dk7.dk@gmail.com>
2024-01-31 13:43:19 +0300

    getting rid of prisma

--- src/app/api/chats/route.ts
+++ src/app/api/chats/route.ts
@@ -1,5 +1,5 @@
 import { db } from "@/lib/db";
-import prisma from "@/lib/db/prisma";
+//import prisma from "@/lib/db/prisma";
 import { Note, notes } from "@/lib/db/schema";
 import openai, { getEmbedding } from "@/lib/openai";
 import { notesIndex } from "@/lib/vectordb";
--- src/app/api/notes2/route.ts
+++ src/app/api/notes2/route.ts
@@ -1,5 +1,5 @@
 import { db } from "@/lib/db";
-import prisma from "@/lib/db/prisma";
+//import prisma from "@/lib/db/prisma";
 import { notes } from "@/lib/db/schema";
 import { getEmbedding } from "@/lib/openai";
 import {
