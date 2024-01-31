c3c25fb72ef9bcbb89efd42eda37e40518109c84
kippytech <dkipyegon.dk7.dk@gmail.com>
2024-01-31 13:26:55 +0300

    deleted prisma api routes

--- src/app/api/getMessages/route.ts
+++ src/app/api/getMessages/route.ts
@@ -1,21 +1,21 @@
-// import { auth } from "@clerk/nextjs";
-// import { NextResponse } from "next/server";
-// import prisma from "@/lib/db/prisma";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+//import prisma from "@/lib/db/prisma";
 
-// export const runtime = "edge";
+export const runtime = "edge";
 
-// export async function GET() {
-//   // const body = await req.json()
+export async function GET() {
+  // const body = await req.json()
 
-//   // const {} = body
+  // const {} = body
 
-//   const { userId } = auth();
+  const { userId } = auth();
 
-//   const messages = await prisma.message.findMany({
-//     where: {
-//       userId: userId,
-//     },
-//   });
+  //   const messages = await prisma.message.findMany({
+  //     where: {
+  //       userId: userId,
+  //     },
+  //   });
 
-//   return NextResponse.json(messages);
-// }
+  return NextResponse.json(userId);
+}
