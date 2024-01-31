aaef6714c5169a92d45777fe6ef3a6a70d47c446
kippytech <dkipyegon.dk7.dk@gmail.com>
2024-01-31 13:02:20 +0300

    commented out prisma in api routes

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,111 +1,111 @@
-import prisma from "@/lib/db/prisma";
-import openai, { getEmbedding } from "@/lib/openai";
-import { notesIndex } from "@/lib/vectordb";
-import { auth } from "@clerk/nextjs";
-//import { ChatCompletionMessage } from "openai/resources/index.mjs";
-import { OpenAIStream, StreamingTextResponse } from "ai";
-import {
-  ChatCompletionRequestMessage,
-  Configuration,
-  OpenAIApi,
-} from "openai-edge";
+// import prisma from "@/lib/db/prisma";
+// import openai, { getEmbedding } from "@/lib/openai";
+// import { notesIndex } from "@/lib/vectordb";
+// import { auth } from "@clerk/nextjs";
+// //import { ChatCompletionMessage } from "openai/resources/index.mjs";
+// import { OpenAIStream, StreamingTextResponse } from "ai";
+// import {
+//   ChatCompletionRequestMessage,
+//   Configuration,
+//   OpenAIApi,
+// } from "openai-edge";
 
-export const runtime = "edge";
+// export const runtime = "edge";
 
-const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+// const config = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
 
-const openai2 = new OpenAIApi(config);
+// const openai2 = new OpenAIApi(config);
 
-export async function POST(req: Request) {
-  try {
-    const body = await req.json();
+// export async function POST(req: Request) {
+//   try {
+//     const body = await req.json();
 
-    const messages: ChatCompletionRequestMessage[] = body.messages;
-    // const messages: ChatCompletionMessage[] = body.messages;
+//     const messages: ChatCompletionRequestMessage[] = body.messages;
+//     // const messages: ChatCompletionMessage[] = body.messages;
 
-    const truncatedMessages = messages.slice(-6);
+//     const truncatedMessages = messages.slice(-6);
 
-    const embedding = await getEmbedding(
-      truncatedMessages.map((mssg) => mssg.content).join("\n"),
-    );
+//     const embedding = await getEmbedding(
+//       truncatedMessages.map((mssg) => mssg.content).join("\n"),
+//     );
 
-    const { userId } = auth();
+//     const { userId } = auth();
 
-    const vectorQueryResponse = await notesIndex.query({
-      vector: embedding,
-      topK: 4,
-      filter: { userId },
-    });
+//     const vectorQueryResponse = await notesIndex.query({
+//       vector: embedding,
+//       topK: 4,
+//       filter: { userId },
+//     });
 
-    const relevantNotes = await prisma.note.findMany({
-      where: {
-        id: {
-          in: vectorQueryResponse.matches.map((match) => match.id),
-        },
-      },
-    });
+//     const relevantNotes = await prisma.note.findMany({
+//       where: {
+//         id: {
+//           in: vectorQueryResponse.matches.map((match) => match.id),
+//         },
+//       },
+//     });
 
-    const systemMessage: ChatCompletionRequestMessage = {
-      role: "assistant",
-      content:
-        "You are an intelligent note-taking app. You answer the users's question based on their existing notes. " +
-        "The relevant notes for this query are:\n" +
-        relevantNotes
-          .map((note) => `Title: ${note.title}\n\nContent: ${note.content}`)
-          .join("\n\n"),
-    };
-    // const systemMessage: ChatCompletionMessage = {
-    //   role: "assistant",
-    //   content:
-    //     "You are an intelligent note-taking app. You answer the users's question based on their existing notes. " +
-    //     "The relevant notes for this query are:\n" +
-    //     relevantNotes
-    //       .map((note) => `Title: ${note.title}\n\nContent: ${note.content}`)
-    //       .join("\n\n"),
-    // };
+//     const systemMessage: ChatCompletionRequestMessage = {
+//       role: "assistant",
+//       content:
+//         "You are an intelligent note-taking app. You answer the users's question based on their existing notes. " +
+//         "The relevant notes for this query are:\n" +
+//         relevantNotes
+//           .map((note) => `Title: ${note.title}\n\nContent: ${note.content}`)
+//           .join("\n\n"),
+//     };
+//     // const systemMessage: ChatCompletionMessage = {
+//     //   role: "assistant",
+//     //   content:
+//     //     "You are an intelligent note-taking app. You answer the users's question based on their existing notes. " +
+//     //     "The relevant notes for this query are:\n" +
+//     //     relevantNotes
+//     //       .map((note) => `Title: ${note.title}\n\nContent: ${note.content}`)
+//     //       .join("\n\n"),
+//     // };
 
-    const res = await openai2.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      temperature: 0,
-      stream: true,
-      messages: [systemMessage, ...truncatedMessages],
-    });
-    // const res = await openai.chat.completions.create({
-    //   model: "gpt-3.5-turbo",
-    //   temperature: 0,
-    //   stream: true,
-    //   messages: [systemMessage, ...truncatedMessages],
-    // });
+//     const res = await openai2.createChatCompletion({
+//       model: "gpt-3.5-turbo",
+//       temperature: 0,
+//       stream: true,
+//       messages: [systemMessage, ...truncatedMessages],
+//     });
+//     // const res = await openai.chat.completions.create({
+//     //   model: "gpt-3.5-turbo",
+//     //   temperature: 0,
+//     //   stream: true,
+//     //   messages: [systemMessage, ...truncatedMessages],
+//     // });
 
-    // const stream = OpenAIStream(res);
-    const stream = OpenAIStream(res, {
-      //save user message to db for chat log persistency
-      onStart: async () => {
-        await prisma.message.create({
-          data: {
-            text: messages[messages.length - 1].content!,
-            isUserMessage: true,
-            userId,
-          },
-        });
-      },
-      //save ai messages for persistency too
-      async onCompletion(completion) {
-        await prisma.message.create({
-          data: {
-            text: completion,
-            isUserMessage: false,
-            userId,
-          },
-        });
-      },
-    });
+//     // const stream = OpenAIStream(res);
+//     const stream = OpenAIStream(res, {
+//       //save user message to db for chat log persistency
+//       onStart: async () => {
+//         await prisma.message.create({
+//           data: {
+//             text: messages[messages.length - 1].content!,
+//             isUserMessage: true,
+//             userId,
+//           },
+//         });
+//       },
+//       //save ai messages for persistency too
+//       async onCompletion(completion) {
+//         await prisma.message.create({
+//           data: {
+//             text: completion,
+//             isUserMessage: false,
+//             userId,
+//           },
+//         });
+//       },
+//     });
 
-    return new StreamingTextResponse(stream);
-  } catch (error) {
-    console.error(error);
-    return Response.json({ error: "Internal server error" }, { status: 500 });
-  }
-}
+//     return new StreamingTextResponse(stream);
+//   } catch (error) {
+//     console.error(error);
+//     return Response.json({ error: "Internal server error" }, { status: 500 });
+//   }
+// }
